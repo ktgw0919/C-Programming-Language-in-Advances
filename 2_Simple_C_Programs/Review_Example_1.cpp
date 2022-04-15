996159c3aced6dafa2c179c71bdbf590501c389a
A21TN013 <103756238+ktgw0919@users.noreply.github.com>
2022-04-16 01:53:44 +0900

    Add files via upload

---
+++ 2_Simple_C_Programs/Review_Example_1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(void){
+    int a;
+    cout<<"Please enter one integer:";
+    cin>>a;
+    cout<<"The integer you enterd is '"
+        <<a<<"'."<<endl;
+    return 0;
+}
