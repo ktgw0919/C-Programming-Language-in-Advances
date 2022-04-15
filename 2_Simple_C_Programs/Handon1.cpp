164fd2c41ceeb97cf64b5e00cc179d4cb915351b
ktgw0919 <103756238+ktgw0919@users.noreply.github.com>
2022-04-16 01:19:43 +0900

    Add files via upload

---
+++ 2_Simple_C_Programs/Handon1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+typedef long long ll;
+
+vector<ll> a;
+int n;
+
+int main(void){
+    cout<<"How many integers to enter?"<<endl;
+    cout<<"Note: Must be less than 2147483647 pieces"<<endl;
+    cin>>n;
+    a.resize(n);
+
+    cout<<"Please enter one integer per line"<<endl;
+    for(int i=0;i<n;i++)cin>>a[i];
+    sort(a.begin(),a.end());
+
+    cout<<endl;
+    cout<<"Sort Results"<<endl;
+    for(int i=0;i<n;i++)cout<<a[i]<<endl;
+
+}
