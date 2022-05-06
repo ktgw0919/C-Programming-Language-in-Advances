166645d3710f0f5f8e1f75c8bc9d39478b49b593
A21TN013 <103756238+ktgw0919@users.noreply.github.com>
2022-05-06 16:06:25 +0900

    Add files via upload

---
+++ 4_pointer_in_Overview_2/Handon4_3_2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+float max_3(float a, float b, float c){
+    if(a>b && a>c)return a;
+    if(b>c && b>a)return b;
+    if(c>a && c>b)return c;
+}
+
+int main(void){
+    float a,b,c;
+    scanf("%f%f%f",&a,&b,&c);
+    printf("%f\n",max_3(a,b,c));
+
+    return 0;
+}
