4b40e3518912fb39c6790ba0fe0a290206e0825e
A21TN013 <103756238+ktgw0919@users.noreply.github.com>
2022-05-06 16:20:42 +0900

    Add files via upload

---
+++ 4_pointer_in_Overview_2/Handon4_4_2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+float mul_2(float a,float b){
+    return a*b;
+}
+
+int main(void){
+    float a,b;
+    scanf("%f%f",&a,&b);
+
+    printf("A*B = %f\n",mul_2(a,b));
+
+    return 0;
+}
